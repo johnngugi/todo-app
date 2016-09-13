f78513688d50d060a30235292ee52e3032d7e160
johnngugi <johnngugi777@gmail.com>
2016-09-13 11:32:36 +0300

    Add templates

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import Form
-from wtforms import SubmitField, TextAreaField, StringField, RadioField
+from wtforms import SubmitField, TextAreaField, SelectField, RadioField
 from wtforms.validators import DataRequired, Regexp
 
 
 class NewTask(Form):
     task = TextAreaField('Task')
-    category = RadioField('Category', choices=[('1', 'Work'), ('2', 'Home')])
-    priority = RadioField('Category', choices=[('3', 'high'), ('2', 'medium'), ('1', 'low')])
+    category = SelectField('Category', choices=[('1', 'Work'), ('2', 'Home')])
+    priority = SelectField('Category', choices=[('3', 'high'), ('2', 'medium'), ('1', 'low')])
     description = TextAreaField('Description')
     submit = SubmitField('Submit')
