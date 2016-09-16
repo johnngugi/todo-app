9393b093e4f1b0ff1cc6d49c483129b5ed1ee89a
johnngugi <johnngugi777@gmail.com>
2016-09-17 00:58:53 +0300

    Add styling changes

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
 	// alert("Hello World");
 	$('select').material_select('destroy');
 	$('select').material_select();
+	$('.button-collapse').sideNav({'edge': 'left'});
 
 });
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    DEBUG = True
+    # DEBUG = True
     WTF_CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -14,9 +14,14 @@ class Config:
         pass
 
 
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://john:root@localhost/todo'
+
+
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://john:root@localhost/todo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-config = {'production': ProductionConfig}
+config = {'production': ProductionConfig,
+          'development': DevelopmentConfig}
